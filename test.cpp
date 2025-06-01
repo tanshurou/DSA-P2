3e4a1ede0c1677358622a9a5965ed4eebef2e708
Lee28092004 <wallace2809@gmail.com>
2025-06-02 02:06:37 +0800

    Done liao task 3 confirm

--- test.cpp
+++ test.cpp
@@ -1,15 +1,8 @@
-// ----------- test.cpp  (“test main”) -----------
-#include "ResultLogger.hpp"
+#include "SpectatorManager.hpp"
+#include <iostream>
 
-int main(int argc, char* argv[]) {
-    // By default, look for "matches.csv" in the current folder.
-    // You can override by passing a different path as the first argument.
-    std::string csvPath = "matches.csv";
-    if (argc > 1) {
-        csvPath = argv[1];
-    }
-
-    // “10” is the size of the “most recent” circular buffer.
-    runResultLogger(csvPath, 10);
+int main() {
+    SpectatorManager specManager;
+    specManager.run();
     return 0;
 }
